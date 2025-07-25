b4ab8418be54cf8274d5df04429a5fcc5262c5e0
wx200010 <wx200010@gmail.com>
2025-07-25 21:05:25 +0800

    Add hashtable implementation using linear probing
    This hashtable uses linear probing to resolve collisions, and since it
    is simple and easy to implement, I add it in.
    
    Be careful that linear probing will cause significant clustering issues.

---
+++ hash/linear_probe/linear_probe.h
@@ -0,0 +1,23 @@
+#ifndef DOUBLE_HASH_H
+#define DOUBLE_HASH_H
+
+#include <stdint.h>
+#include <limits.h>
+#define TABLE_SIZE 100007
+#define EMPTY INT_MIN
+#define DELETED INT_MAX
+typedef struct
+{
+    int key; // when the key is set to INT_MIN, it means that the hash item is unoccupied now.
+    int value;
+} hash_item_t;
+
+int hash1(int key);
+int hash2(int key);
+hash_item_t *hashtable_create();
+hash_item_t *hashtable_search(hash_item_t *ht, int key);
+void hashtable_insert(hash_item_t *ht, int key, int value);
+void hashtable_remove(hash_item_t *ht, int key);
+void hashtable_print_table(hash_item_t *ht);
+
+#endif // DOUBLE_HASH_H
